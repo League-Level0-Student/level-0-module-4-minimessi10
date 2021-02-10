63f474a188e1ee19f1057c3c47110d7629a7a35c
student <student@ip-172-31-61-139.us-west-2.compute.internal>
2021-02-09 18:25:44 -0800

    noice

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,10 +5,10 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+		String value = "Lionel Messi";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-
+		System.out.println(value.charAt(2));
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
