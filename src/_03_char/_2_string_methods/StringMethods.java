82c873b99d80d64b70d0ceff97a03e9bcb149d03
student <student@ip-172-31-62-6.us-west-2.compute.internal>
2021-02-16 18:27:10 -0800

    cool

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -12,19 +12,27 @@ public class StringMethods {
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(value.length());
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for (int i = 0; i < value.length(); i++) {
+	System.out.println(value.charAt(i));
+}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+for (int i = 0; i < value.length(); i++) {
+	if(value.charAt(i)=='M') {
+		System.out.println(i);
 	}
 }
+	
+	}
+	}
+
 
 
